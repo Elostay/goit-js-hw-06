2ad285fdbf776a75f503bda873d3cf7e94b6d162
Elostay <nastia60203@gmail.com>
2023-09-04 09:56:56 +0300

    final

--- js/task-10.js
+++ js/task-10.js
@@ -1,11 +1,14 @@
-//!                     НЕ ЗРОБЛЕНО
-
 const input = document.querySelector('input[type="number"]');
 const create = document.querySelector('button[data-create]');
 const destroy = document.querySelector('button[data-destroy]');
 const boxes = document.getElementById('boxes');
 
-// destroy.addEventListener('click');
+create.addEventListener('click', function () {
+  var amount = parseInt(input.value);
+  createBoxes(amount);
+});
+
+destroy.addEventListener('click', destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -13,14 +16,27 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-let counter = 0;
-function inputListener(event) {
-  return Number(event.currentTarget.value);
-}
-console.log(counter);
-
 function createBoxes(amount) {
-  //   console.log(amount);
+  if (amount < 0 || amount > 100) {
+    alert('Будь-ласка введіть значення від 1 до 100');
+  }
+  let size = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.borderRadius = `50%`;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    size += 10;
+    boxes.append(div);
+  }
 }
-input.addEventListener('input', inputListener);
-create.addEventListener('click', createBoxes(counter));
+
+function inputCleaner(inp) {
+  inp.value = '';
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+  inputCleaner(input);
+}
